541138c74b6a5868e04584c235fdf1bc41210a60
cvega <carlosjuniorrvega@hotmail.com>
2025-11-19 16:12:37 -0500

    chore(fundamentals): add challenge description in chall_07_smallest_number_in_array. CV

--- src/main/java/fundamentals/chall_07_smallest_number_in_array/FindSmallestNumber.java
+++ src/main/java/fundamentals/chall_07_smallest_number_in_array/FindSmallestNumber.java
@@ -1,3 +1,8 @@
+/*
+Write a Java method named returnSmallest that accepts an integer array (int[] arr) as input and returns an integer.
+The method must find and return the smallest numerical value (the minimum) contained within the array.
+The method should assume the array is not empty and must correctly compare both positive and negative integers.
+ */
 package fundamentals.chall_07_smallest_number_in_array;
 
 public class FindSmallestNumber {
